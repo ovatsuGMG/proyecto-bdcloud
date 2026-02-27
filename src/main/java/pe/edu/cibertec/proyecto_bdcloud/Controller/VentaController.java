d3fba136e53fccf6963d3757c7179bdffd3a1d35
ovatsuGMG <tu-correo@ejemplo.com>
2026-02-26 19:41:02 -0500

    Agregando estilo

--- src/main/java/pe/edu/cibertec/proyecto_bdcloud/Controller/VentaController.java
+++ src/main/java/pe/edu/cibertec/proyecto_bdcloud/Controller/VentaController.java
@@ -1,18 +1,18 @@
 package pe.edu.cibertec.proyecto_bdcloud.Controller;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import pe.edu.cibertec.proyecto_bdcloud.model.Venta;
-import pe.edu.cibertec.proyecto_bdcloud.model.Videojuego;
 import pe.edu.cibertec.proyecto_bdcloud.service.VentaService;
-import pe.edu.cibertec.proyecto_bdcloud.service.VideojuegoService;
 
 import java.util.List;
 
 @RestController
 @RequestMapping("/api/ventas")
+@CrossOrigin(origins = "*")
 public class VentaController {
     private final VentaService ventaService;
 
